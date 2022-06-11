e5f7256a1ddce7be93315fd4e4f61785950c3936
sayudhalw <mazaya.dev@gmail.com>
2022-06-11 18:11:55 +0700

    + Optimize Vote Sorting

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 class ProductList extends React.Component {
 	render() {
 		const products = Seed.products.sort((a, b) => b.votes - a.votes);
-		const productComponents = Seed.products.map((product) => (
+		const productComponents = products.map((product) => (
 			<Product
 				key={'product-' + product.id}
 				id={product.id}
